e2ad15a2d2651bad4bd44105f757778ae35456b0
alumno <juanfdg1987@gmail.com>
2017-01-13 13:34:06 +0100

    Hibernate creada la inserccion

--- src/main/java/com/mycompany/neodatisdboejemplo1/EjemploNeoDatis.java
+++ src/main/java/com/mycompany/neodatisdboejemplo1/EjemploNeoDatis.java
@@ -5,11 +5,15 @@
  */
 package com.mycompany.neodatisdboejemplo1;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Scanner;
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
 import org.neodatis.odb.OID;
+import org.neodatis.odb.ObjectValues;
 import org.neodatis.odb.Objects;
+import org.neodatis.odb.Values;
 import org.neodatis.odb.core.oid.OIDFactory;
 import org.neodatis.odb.core.query.IQuery;
 import org.neodatis.odb.core.query.criteria.And;
@@ -17,12 +21,14 @@ import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Or;
 import org.neodatis.odb.core.query.criteria.Where;
 import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
+import org.neodatis.odb.impl.core.query.values.ValuesCriteriaQuery;
 
 /**
  *
  * @author alumno
  */
 public class EjemploNeoDatis {
+
     public static void main(String[] args) {
 	int opcion = 0;
 	Scanner sc = new Scanner(System.in);
@@ -66,12 +72,16 @@ public class EjemploNeoDatis {
 		    ejemplosConsultasBDOC();
 		    break;
 		}
-		case 7:{
+		case 7: {
 		    consultasVariosParametros();
 		    break;
 		}
-		case 8:{
+		case 8: {
 		    actualizacionPais();
+		    break;
+		}
+		case 9: {
+		    consultasDeAgregado();
 		    break;
 		}
 		default: {
@@ -82,81 +92,81 @@ public class EjemploNeoDatis {
     }
 
     private static void insercionDatosEjemplo() {
-	Jugadores jugador1 = new Jugadores("Maria","Voleybol","Madrid",14,new Pais(1,"Francia"));
-	Jugadores jugador2 = new Jugadores("Miguel","Tenis","Valencia",13,null);
-	Jugadores jugador3 = new Jugadores("Mario","Baloncesto","Guadalajara",15,null);
-	Jugadores jugador4 = new Jugadores("Alicia","Tenis","Madrid",15,new Pais(2,"Spain"));
-	
+	Jugadores jugador1 = new Jugadores("Maria", "Voleybol", "Madrid", 14, new Pais(1, "Francia"));
+	Jugadores jugador2 = new Jugadores("Miguel", "Tenis", "Valencia", 13, null);
+	Jugadores jugador3 = new Jugadores("Mario", "Baloncesto", "Guadalajara", 15, null);
+	Jugadores jugador4 = new Jugadores("Alicia", "Tenis", "Madrid", 15, new Pais(2, "Spain"));
+
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	odb.store(jugador1);
 	odb.store(jugador2);
 	odb.store(jugador3);
 	odb.store(jugador4);
-	
+
 	odb.commit();
-	
+
 	Objects<Jugadores> objects = odb.getObjects(Jugadores.class);
-	System.out.println("Se han recuperado "+objects.size()+" resultados");
+	System.out.println("Se han recuperado " + objects.size() + " resultados");
 	int num = 0;
-	while(objects.hasNext()){
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    num++;
-	    System.out.println("Jugador "+num+"\n");
+	    System.out.println("Jugador " + num + "\n");
 	    System.out.println(jugador);
 	}
-	
+
 	odb.close();
     }
 
     private static void accesoPorOID() {
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	OID oid = OIDFactory.buildObjectOID(3);
-	
+
 	Jugadores jugador = (Jugadores) odb.getObjectFromId(oid);
-	
+
 	System.out.println(jugador);
-	
+
 	odb.close();
     }
 
     private static void consultaSencillaTenis() {
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	IQuery query = new CriteriaQuery(Jugadores.class, Where.equal("deporte", "Tenis"));
-	
+
 	query.orderByAsc("nombre,edad");
-	
+
 	Objects<Jugadores> objects = odb.getObjects(query);
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	}
-	
+
 	odb.close();
     }
 
     private static void actualizacionDeObjetos() {
 	Scanner sc = new Scanner(System.in);
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	System.out.print("Introduce el nombre del jugador a modificar: ");
-	
+
 	String nombre = sc.nextLine();
-	
+
 	IQuery query = new CriteriaQuery(Jugadores.class, Where.equal("nombre", nombre));
-	
+
 	Jugadores jugador = (Jugadores) odb.getObjects(query).getFirst();
-	
+
 	System.out.print("Introduce nuevo deporte: ");
 	String deporte = sc.nextLine();
-	
+
 	jugador.setDeporte(deporte);
-	
+
 	System.out.println(jugador);
-	
+
 	odb.store(jugador);
 	odb.commit();
 	odb.close();
@@ -165,15 +175,15 @@ public class EjemploNeoDatis {
     private static void borradoObjectos() {
 	Scanner sc = new Scanner(System.in);
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	System.out.print("Introduce el nombre del jugador a borrar: ");
-	
+
 	String nombre = sc.nextLine();
-	
+
 	IQuery query = new CriteriaQuery(Jugadores.class, Where.equal("nombre", nombre));
-	
+
 	Jugadores jugador = (Jugadores) odb.getObjects(query).getFirst();
-	
+
 	odb.delete(jugador);
 	odb.commit();
 	odb.close();
@@ -183,12 +193,12 @@ public class EjemploNeoDatis {
 	ODB odb = ODBFactory.open("equipos.db");
 	ICriterion criterion = Where.like("nombre", "M%");
 	IQuery query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	Objects<Jugadores> objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuyo nombre empieza por M\n");
 	System.out.println("-----------------------------------");
-	while(objects.hasNext()){
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -196,13 +206,13 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.not(Where.like("nombre", "M%"));
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuyo nombre no empieza por M\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -210,13 +220,13 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.ge("edad", 14);
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuya edad sea mayor o igual a 14 años\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -224,13 +234,13 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.le("edad", 14);
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuya edad sea menor o igual a 14 años\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -238,13 +248,13 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.lt("edad", 14);
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuya edad sea menor a 14 años\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -252,13 +262,13 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.gt("edad", 14);
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores cuya edad sea mayor a 14 años\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
@@ -266,92 +276,92 @@ public class EjemploNeoDatis {
 	System.out.println("\n");
 	criterion = Where.isNotNull("ciudad");
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
+
 	System.out.println("Jugadores que tienen una residencia asignada\n");
 	System.out.println("-----------------------------------");
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
 	}
 	System.out.println("\n");
-	
+
 	System.out.println("Jugadores no tienen una nacionalidad\n");
 	System.out.println("-----------------------------------");
 	criterion = Where.isNull("pais");
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
-	
-	while(objects.hasNext()){
+
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
 	}
-	
+
 	odb.close();
     }
 
     private static void consultasVariosParametros() {
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	ICriterion criterion = new And()
 		.add(Where.equal("ciudad", "Madrid"))
 		.add(Where.ge("edad", 15));
-	
+
 	IQuery query = new CriteriaQuery(Jugadores.class, criterion);
 	Objects<Jugadores> objects = odb.getObjects(query);
 	System.out.println("Jugadores que tienen 15 o mas años y viven en Madrid");
 	System.out.println("----------------------------------------------------");
-	while(objects.hasNext()){
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
 	}
 	System.out.println();
-	
+
 	criterion = new And()
 		.add(Where.equal("ciudad", "Madrid"))
 		.add(new Or()
 			.add(Where.equal("deporte", "Baloncesto"))
 			.add(Where.equal("deporte", "Tenis"))
 		);
-	
+
 	query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	objects = odb.getObjects(query);
 	System.out.println("Jugadores que juegan al Baloncesto o Tenis y viven en Madrid");
 	System.out.println("----------------------------------------------------");
-	while(objects.hasNext()){
+	while (objects.hasNext()) {
 	    Jugadores jugador = objects.next();
 	    System.out.println(jugador);
 	    System.out.println("");
 	}
 	System.out.println();
-	
+
 	odb.close();
 
     }
 
     private static void actualizacionPais() {
 	ODB odb = ODBFactory.open("equipos.db");
-	
+
 	ICriterion criterion = Where.equal("pais.nombrePais", "Francia");
-	
+
 	IQuery query = new CriteriaQuery(Jugadores.class, criterion);
-	
+
 	Objects<Jugadores> objects = odb.getObjects(query);
-	
-	if(objects.size()==0){
+
+	if (objects.size() == 0) {
 	    System.out.println("No hay jugadores que actualizar");
-	}else{
-	    while(objects.hasNext()){
+	} else {
+	    while (objects.hasNext()) {
 		Jugadores jugador = objects.next();
-		jugador.setEdad(jugador.getEdad()+1);
-		System.out.println("El jugador "+jugador.getNombre()+" se ha actualizado, a continuación aparecen sus datos");
+		jugador.setEdad(jugador.getEdad() + 1);
+		System.out.println("El jugador " + jugador.getNombre() + " se ha actualizado, a continuación aparecen sus datos");
 		System.out.println(jugador);
 		System.out.println();
 		odb.store(jugador);
@@ -360,5 +370,64 @@ public class EjemploNeoDatis {
 	}
 	odb.close();
     }
-}
 
+    private static void consultasDeAgregado() {
+	ODB odb = ODBFactory.open("equipos.db");
+
+	Values valores = odb.getValues(new ValuesCriteriaQuery(Jugadores.class).field("nombre").field("ciudad"));
+
+	while (valores.hasNext()) {
+	    ObjectValues objectValue = valores.next();
+	    System.out.println("Nombre: " + objectValue.getByAlias("nombre") + " Ciudad: " + objectValue.getByIndex(1));
+	}
+	System.out.println();
+	System.out.println("Obtenga la suma de las edades de los jugadores");
+	System.out.println("----------------------------------------------");
+
+	Values valores1 = odb.getValues(new ValuesCriteriaQuery(Jugadores.class).sum("edad"));
+
+	ObjectValues ovl = valores1.nextValues();
+
+	BigDecimal sumaEdades = (BigDecimal) ovl.getByAlias("edad");
+	System.out.println("La suma de las edades de los jugadores es: " + sumaEdades.longValue());
+	System.out.println();
+
+	System.out.println("Obtenga la media de las edades de los jugadores");
+	System.out.println("----------------------------------------------");
+	Values valores2 = null;
+	ObjectValues ovl1 = null;
+	try {
+	    valores2 = odb.getValues(new ValuesCriteriaQuery(Jugadores.class).avg("edad"));
+	    ovl1 = valores2.nextValues();
+	    BigDecimal mediaEdades = (BigDecimal) ovl.getByAlias("edad");
+	    
+	    System.out.println("La media de las edades de los jugadores es: " + mediaEdades.longValue());
+	} catch (ArithmeticException ae) {
+	    valores2 = odb.getValues(new ValuesCriteriaQuery(Jugadores.class).sum("edad").count("edad"));
+	    ovl1 = valores2.nextValues();
+	    BigDecimal sumaPufEdades = (BigDecimal) ovl1.getByIndex(0);
+	    BigInteger cuentaDeJugadores = (BigInteger) ovl1.getByIndex(1);
+	    double media = sumaPufEdades.floatValue() / cuentaDeJugadores.intValue();
+
+	    System.out.println("La media de las edades de los jugadores es: " + media);
+	}
+
+	System.out.println();
+
+	System.out.println("Obtenga la edad minima y maxima de los jugadores y el numero de jugadores");
+	System.out.println("----------------------------------------------");
+
+	Values valores3 = odb.getValues(new ValuesCriteriaQuery(Jugadores.class).min("edad").count("nombre").max("edad"));
+
+	ObjectValues ovl2 = valores3.nextValues();
+
+	BigDecimal edadMin = (BigDecimal) ovl2.getByIndex(0);
+	BigInteger cantidad = (BigInteger) ovl2.getByIndex(1);
+	BigDecimal edadMax = (BigDecimal) ovl2.getByIndex(2);
+	System.out.println("Edad minima: " + edadMin + " Edad maxima: " + edadMax + " Cantidad jugadores: " + cantidad);
+	System.out.println();
+
+	odb.close();
+
+    }
+}
